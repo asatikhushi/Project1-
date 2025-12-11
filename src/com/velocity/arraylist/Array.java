74987cb856d4c2e9e004ce2c7ac9c032896efb50
Simran <simranbhaisare649@gmail.com>
2025-12-11 15:42:21 +0530

    Modify

--- src/com/velocity/arraylist/Array.java
+++ src/com/velocity/arraylist/Array.java
@@ -1,5 +1,22 @@
 package com.velocity.arraylist;
 
+import java.util.Scanner;
+
 public class Array {
-    
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+     int num1[]  =new int[4] ; 
+     int result[]  =new int[4] ;
+     System.out.println("Enter integer value ");
+     for(int i=0;i<num1.length;i++){
+        num1[i]=sc.nextInt();
+     }
+      sc.close();
+
+     System.out.println("print integer value which is enter by user :");
+     for(int i=0;i<num1.length;i++){
+       result[i] =num1[i];
+         System.out.println(result[i]);
+     }
+    }     
 }
