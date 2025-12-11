4aa76bec97c326b024c3ad7635fa2d19316aaf2a
Simran <simranbhaisare649@gmail.com>
2025-12-11 15:08:20 +0530

    Modify

--- src/com/velocity/arraylist/Array1.java
+++ src/com/velocity/arraylist/Array1.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 public class Array1 {
     public static void main(String[] args) {
      Scanner sc=new Scanner(System.in);
-     int num[]  =new int[4] ;
+     int num1[]  =new int[4] ;
+     int num2[]  =new int[4] ;
+     int result[]  =new int[4] ;
+
+     System.out.println("Enter integer value ");
+     for(int i=0;i<num1.length;i++){
+        num1[i]=sc.nextInt();
+     }
 
      System.out.println("Enter integer value");
-     for(int i=0;i<num.length;i++){
-        num[i]=sc.nextInt();
+     for(int i=0;i<num1.length;i++){
+        num2[i]=sc.nextInt();
      }
      sc.close();
-
-      System.out.println();
-      System.out.println("print integer value which is enter by user :");
-      for(int i=0;i<num.length;i++){
-       System.out.println(num[i]) ;
+     System.out.println("print sum of two integer value which is enter by user :");
+     for(int i=0;i<num1.length;i++){
+       result[i] =num1[i]+num2[i];
+         System.out.println(result[i]);
      }
     }
 }
